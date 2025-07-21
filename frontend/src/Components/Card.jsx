2fa59ab75a035806588d41868a95f48426205514
sudharshan-03 <sudha37737@gmail.com>
2025-07-21 15:10:23 +0530

    time updated

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -6,16 +6,38 @@ import build from "../assets/build.svg";
 import salary from "../assets/salary.svg";
 
 function Card({ job }) {
-  const { image, position, salaryRangeTo, description } = job;
+  const { image, position, salaryRangeTo, description,createdAt } = job;
+
+  function getTimeAgo(createdTime) {
+    const createdDate = new Date(createdTime);
+    const now = new Date();
+
+    const diffMs = now - createdDate; // difference in milliseconds
+    const diffSec = Math.floor(diffMs / 1000);
+    const diffMin = Math.floor(diffSec / 60);
+    const diffHours = Math.floor(diffMin / 60);
+    const diffDays = Math.floor(diffHours / 24);
+
+    if (diffDays > 0) return `${diffDays} day${diffDays > 1 ? "s" : ""} Ago`;
+    if (diffHours > 0)
+      return `${diffHours} hour${diffHours > 1 ? "s" : ""} Ago`;
+    if (diffMin > 0) return `${diffMin} minute${diffMin > 1 ? "s" : ""} Ago`;
+    return `Just now`;
+  }
 
   return (
     <>
       <div className="cardContainer">
         <div className="imageContent">
           <div className="imgContainer">
-            <img src={`https://jobmanagement-v40a.onrender.com/logos/${job.image.split("/").pop()}`} alt="logo" />
+            <img
+              src={`https://jobmanagement-v40a.onrender.com/logos/${job.image
+                .split("/")
+                .pop()}`}
+              alt="logo"
+            />
           </div>
-          <div className="time">24h Ago</div>
+          <div className="time">{getTimeAgo(job.createdAt)}</div>
         </div>
         <h2 className="heading">{position}</h2>
         <div className="details">
@@ -34,7 +56,7 @@ function Card({ job }) {
         </div>
 
         <ul className="description">
-          {description.split('. ').map((desc, index) => (
+          {description.split(". ").map((desc, index) => (
             <li key={index}>{desc}</li>
           ))}
         </ul>
